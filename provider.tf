eda256016c7523a2ff589e2d6e7fd10c2440d95d
Great-Stone <hahohh@gmail.com>
2024-10-14 19:57:29 +0900

    region setup from variables.tf

--- provider.tf
+++ provider.tf
@@ -17,5 +17,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-northeast-2"
+  region = var.region
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  default = "ap-northeast-2"
+  description = "AWS Region"
+}
+
 variable "instance_type" {
   default = "m7i-flex.2xlarge"
   description = "Instance type to use for the instance."
